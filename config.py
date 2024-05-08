614a6db48abf7ff0b215c92df7d51e6d2465f0d9
BazhenovaOlga <bazhenovaolga09@gmail.com>
2024-05-08 11:39:12 +0300

    начальная версия

---
+++ config.py
@@ -0,0 +1,22 @@
+HOME_DIR = '/home/student/FINAL_prog4'
+LOGS = f'{HOME_DIR}/logs.txt'
+DB_FILE = f'{HOME_DIR}/users.db'
+
+
+IAM_TOKEN_PATH = f'{HOME_DIR}/creds/iam_token.txt'
+FOLDER_ID_PATH = f'{HOME_DIR}/creds/folder_id.txt'
+BOT_TOKEN_PATH = f'{HOME_DIR}/creds/bot_token.txt'
+
+
+
+MAX_USER_STT_BLOCKS = 4
+MAX_USER_TTS_SYMBOLS = 800
+MAX_TTS_SYMBOLS = 400
+MAX_USER_GPT_TOKENS = 500
+MAX_GPT_TOKENS = 120
+MAX_USERS = 3
+
+
+SYSTEM_PROMPT = [{'role': 'system', 'text': 'Ты веселый собеседник. Общайся с пользователем на "ты" и используй юмор. '
+                                            'Поддерживай диалог. Не объясняй пользователю, что ты умеешь и можешь. '
+                                            'Изображай человека. Отвечай кратко'}]
